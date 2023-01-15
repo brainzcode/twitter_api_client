82ac3c568a0b5e5e56e130019c343f27a67586d0
brainzcode <98150590+brainzcode@users.noreply.github.com>
2023-01-15 02:11:07 +0100

    Navbar/Footer Initial Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,64 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<div>
-			<h1>Footer</h1>
-		</div>
+		<footer className='footContainer'>
+			<div className='footer'>
+				<div className='top'>
+					<div className='item'>
+						<h1>Categories</h1>
+						<ul>
+							<li>Women</li>
+							<li>Men</li>
+							<li>Shoes</li>
+							<li>Accessories</li>
+							<li>New Arrivals</li>
+						</ul>
+					</div>
+					<div className='item'>
+						<h1>Links</h1>
+						<ul>
+							<li>FAQs</li>
+							<li>Pages</li>
+							<li>Stores</li>
+							<li>Compare</li>
+							<li>Cookies</li>
+						</ul>
+					</div>
+					<div className='item'>
+						<h1>About</h1>
+						<span>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Blanditiis tempora magnam vitae deleniti fugit
+							beatae laborum cum dolore dolores consequatur.
+							Impedit cum cumque dicta assumenda! Dicta molestiae
+							natus incidunt facilis tempore aliquid, consectetur
+							quis aliquam.
+						</span>
+					</div>
+					<div className='item'>
+						<h1>Contact</h1>
+						<span>
+							Lorem ipsum dolor, sit amet consectetur adipisicing
+							elit. Ad harum non laborum quam facilis consequuntur
+							sequi distinctio ullam minus esse animi dolorem,
+							obcaecati sit voluptatum tempora accusamus eligendi
+							magnam adipisci explicabo repudiandae sed vel est
+							earum.
+						</span>
+					</div>
+				</div>
+				<div className='bottom'>
+					<div className='left'>
+						<span className='logo'>Twitter API</span>
+						<span className='copyRight'>
+							&copy;
+							{new Date().getFullYear()}, All Rights Reserved
+						</span>
+					</div>
+				</div>
+			</div>
+		</footer>
+
 	);
 };
 
